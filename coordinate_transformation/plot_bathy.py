5f4eea513aa54b9e5176c0f487a8dca4a78b21b8
krisjuune <kristiina.joon@gmail.com>
2020-01-13 18:32:14 +0000

    Fix bugs in bathymetry plot script

--- coordinate_transformation/plot_bathy.py
+++ coordinate_transformation/plot_bathy.py
@@ -5,13 +5,11 @@ import pickle
 import numpy as np
 import numpy.ma as ma
 from pathlib import Path
-from transformation_functions.get_domain import find_nearest, truncate_domain
-
-suva = np.arange(10.)
-find_nearest(suva, 5.4)
+from transformation_functions.get_domain import \
+    find_nearest, truncate_domain
 
 # Load the dumped variables
-path = 'topography-transformation/coordinate_transformation/variables/'
+path = 'coordinate_transformation/variables/'
 path = Path(path)
 
 with open(path / 'lat_Prt', 'rb') as f:
@@ -43,6 +41,7 @@ from matplotlib.colorbar import ColorbarBase
 from mpl_toolkits.basemap import Basemap
 
 fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
 fig = Basemap(projection = 'mill', llcrnrlat = lat_min, \
     urcrnrlat = lat_max, llcrnrlon = lon_min, \
     urcrnrlon = lon_max, resolution = 'c')
@@ -50,27 +49,20 @@ fig.drawmapboundary()
 # Draw a lon/lat grid (20 lines for an interval of one degree)
 fig.drawparallels(np.linspace(lat_min, lat_max, num = 5), \
     labels=[1, 0, 0, 0], fmt="%.2f", dashes=[2, 2])
-fig.drawmeridians(np.linspace(-20, -16, 5), labels=[0, 0, 0, 1], \
-    fmt="%.g", dashes=[2, 2])
+fig.drawmeridians(np.arange(round(lon_min), round(lon_max), 1), \
+    labels=[0, 0, 0, 1], fmt="%.2f", dashes=[2, 2])
 
 # Add elevation data to map
 cmap = 'viridis'
 Lon_dom, Lat_dom = np.meshgrid(lon_dom, lat_dom)
-map.pcolormesh(Lon_dom, Lat_dom, elevation_dom, latlon = True, \
+fig.pcolormesh(Lon_dom, Lat_dom, elevation_dom, latlon = True, \
     cmap = cmap)
 # Colorbar construction
-cbar = fig.colorbar(surf, shrink = 0.5, aspect = 5, \
-    orientation = 'vertical')
-cbar.ax.set_clim(5., 0.)
-cbar.set_label('Bathymetry (km)', rotation = 270)
+i = ax.imshow(elevation_dom, interpolation='nearest')
+cbar = fig.colorbar(i, shrink = 0.5, aspect = 5)
+cbar.set_label('Bathymetry (km)', rotation = 270, labelpad=10, y=0.45)
 
-# cax = fig.add_axes([0.27, 0.1, 0.5, 0.05]) # posititon
-# cb = ColorbarBase(cax,cmap=cmap, orientation='horizontal')
-# cb.ax.set_xlabel('Bathymetry in chosen domain (km)')
-
-# plt.axes(ax)
-plt.title('wad is dis')
-# plt.savefig('dom_bathymetry.png', dpi = 600)
+plt.savefig('dom_bathymetry_38N17,5W.png', dpi = 600)
 plt.show()
 
 # %%
