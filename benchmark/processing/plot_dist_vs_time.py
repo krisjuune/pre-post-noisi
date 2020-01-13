5c18a34b68669c4c5bf5a15121ac52c3da77d149
krisjuune <kristiina.joon@gmail.com>
2020-01-13 17:24:23 +0000

    reorganize

--- benchmark/processing/plot_dist_vs_time.py
+++ benchmark/processing/plot_dist_vs_time.py
@@ -1,15 +1,17 @@
 import numpy as np 
 from pathlib import Path
 import matplotlib.pyplot as plt
-from benchmark.processing.plot_validation import station_data
+from plot_validation import station_data
 
 stations_lat = ['LAT2', 'LAT4', 'LAT6', 'LAT8', 'LAT10']
 stations_lon = ['LON2', 'LON4', 'LON6', 'LON8', 'LON10']
-path = benchmark/processing/raw_data/sim1
+path = 'raw_data/sim1'
+
+suva_data = station_data(path, stations_lat [1])
 
 def plot_dist_time(path, stations, save = False, \
     *filename, vertical = True, transverse = False, \
-    radial = False):
+    radial = False, data_points = 500):
     """
     Function to plot seismograms stack atop one another as
     distance vs time, using the function station_data. 
@@ -20,28 +22,30 @@ def plot_dist_time(path, stations, save = False, \
     Choose the component to be plotted, either vertical 
     or transverse. Vertical chosen by default. 
     """
+    fig = plt.figure(figsize = (7,7), dpi = 1200)
 
     for i in np.arange(len(stations)):
-        
         # Get the data from all the stations
         data = station_data(path, stations[i])
-        time = data[:,0]
+        time = data[0:data_points,0]
         # Retrieve the desired comoponent data
         if vertical == True:
-            displacement = data[:,3]
+            displacement = data[0:data_points,3]
         elif transverse == True: 
-            displacement = data[:,2]
+            displacement = data[0:data_points,2]
         elif radial == True:
-            displacement = data[:,1]
+            displacement = data[0:data_points,1]
         else: 
             print('Error, choose displacement component')
         
         # Begin plotting
         if i == 0:
-            plt(time, displacement, linewidth = 1, linestyle = '-', \
-                color = 'orange')
+            plt.plot(time, displacement, linewidth = 1, \
+                linestyle = '-', color = 'orange')
             dx = 0.8*max(displacement)
-        plt.show()
+    return fig
+
+plot_dist_time(path, stations_lat)
 
     
 
--- coordinate_transformation/plot_bathy.py
+++ coordinate_transformation/plot_bathy.py
@@ -4,17 +4,23 @@
 import pickle 
 import numpy as np
 import numpy.ma as ma
-from transformation_functions.get_domain \
-    import find_nearest, truncate_domain
+from pathlib import Path
+from transformation_functions.get_domain import find_nearest, truncate_domain
+
+suva = np.arange(10.)
+find_nearest(suva, 5.4)
 
 # Load the dumped variables
-with open('coordinate_transformation/lat_Prt', 'rb') as f:
+path = 'topography-transformation/coordinate_transformation/variables/'
+path = Path(path)
+
+with open(path / 'lat_Prt', 'rb') as f:
     lat_Prt = pickle.load(f)
 lat_Prt = np.ma.getdata(lat_Prt)
-with open('coordinate_transformation/lon_Prt', 'rb') as f:
+with open(path / 'lon_Prt', 'rb') as f:
     lon_Prt = pickle.load(f)
 lon_Prt = np.ma.getdata(lon_Prt)
-with open('coordinate_transformation/bathy_Prt', 'rb') as f:
+with open(path / 'bathy_Prt', 'rb') as f:
     bathy_Prt = pickle.load(f)
 bathy_Prt = np.ma.getdata(bathy_Prt)
 
@@ -30,21 +36,21 @@ bounds = [lat_max, lat_min, lon_max, lon_min]
 
 elevation_dom = elevation_dom/1000 #plot elevation in km
 
-# %% Plot bathymetry using Basemap
+# %% Plot bathymetry using Basemap, info from ObsPy basemap plots
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colorbar import ColorbarBase
 from mpl_toolkits.basemap import Basemap
 
-# Useful info from ObsPy basemap plots
-map = Basemap(projection = 'mill', llcrnrlat = lat_min, \
+fig = plt.figure()
+fig = Basemap(projection = 'mill', llcrnrlat = lat_min, \
     urcrnrlat = lat_max, llcrnrlon = lon_min, \
     urcrnrlon = lon_max, resolution = 'c')
-map.drawmapboundary()
+fig.drawmapboundary()
 # Draw a lon/lat grid (20 lines for an interval of one degree)
-map.drawparallels(np.linspace(lat_min, lat_max, num = 5), \
+fig.drawparallels(np.linspace(lat_min, lat_max, num = 5), \
     labels=[1, 0, 0, 0], fmt="%.2f", dashes=[2, 2])
-map.drawmeridians(np.linspace(-20, -16, 5), labels=[0, 0, 0, 1], \
+fig.drawmeridians(np.linspace(-20, -16, 5), labels=[0, 0, 0, 1], \
     fmt="%.g", dashes=[2, 2])
 
 # Add elevation data to map
@@ -53,13 +59,18 @@ Lon_dom, Lat_dom = np.meshgrid(lon_dom, lat_dom)
 map.pcolormesh(Lon_dom, Lat_dom, elevation_dom, latlon = True, \
     cmap = cmap)
 # Colorbar construction
+cbar = fig.colorbar(surf, shrink = 0.5, aspect = 5, \
+    orientation = 'vertical')
+cbar.ax.set_clim(5., 0.)
+cbar.set_label('Bathymetry (km)', rotation = 270)
+
 # cax = fig.add_axes([0.27, 0.1, 0.5, 0.05]) # posititon
 # cb = ColorbarBase(cax,cmap=cmap, orientation='horizontal')
 # cb.ax.set_xlabel('Bathymetry in chosen domain (km)')
 
 # plt.axes(ax)
 plt.title('wad is dis')
-plt.savefig('dom_bathymetry.png', dpi = 600)
+# plt.savefig('dom_bathymetry.png', dpi = 600)
 plt.show()
 
 # %%
