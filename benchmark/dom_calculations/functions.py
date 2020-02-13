Fix units issues in relabelling file functions

benchmark/dom_calculations/functions.py
@@ -7,18 +7,17 @@ from coordinate_transformation.functions.get_spherical \
 from coordinate_transformation.functions.get_domain \
     import find_nearest
 
-def get_curvature(lat, lon, radius = 6370.287272978241, \
+def get_curvature(lat, lon, radius = 6370287.272978241, \
     theta = 37.5, phi = -16.5):
     """
-    Function to calculate the curvature 
-    relative to a flat surface at the given 
-    radius assuming a sphere with the given 
-    radius. Inputs include arrays of latitude, 
-    longitude, and radius. Function returns 
-    array of depths relative to this flat 
-    surface with the dimensions of lon, lat.
-    Units in degrees for angles, km for 
-    distances.  
+    Function to calculate the curvature relative to a 
+    flat surface at the given radius assuming a sphere 
+    with the given radius. Inputs include arrays of 
+    latitude, longitude, and a radius. Function returns 
+    array of depths relative to this flat surface with 
+    the dimensions of lon, lat. 
+    Units in degrees for angles, distances same as radius. 
+    Default radius calculated at default geographic theta.   
     """
     # preallocate output array
     curvature = np.zeros((len(lon), \
@@ -49,23 +48,21 @@ def get_curvature(lat, lon, radius = 6370.287272978241, \
             # surface
             y = radius/np.cos(alpha) - radius
             x = y*np.cos(alpha)
-            curvature [i,j] = x 
+            curvature [i,j] = x*(-1)
     
     return(curvature)
 
-def get_curvature_wgs84(lat, lon, radius = 6370.287272978241, \
+def get_curvature_wgs84(lat, lon, radius = 6370287.272978241, \
     theta = 37.5, phi = -16.5):
     """
-    Function to calculate the curvature relative to 
-    a flat surface at the given radius for an 
-    ellipsoid defined by wgs84. Inputs include 
-    arrays of latitude, longitude, and a radius. 
-    Function returns array of depths relative to 
-    this flat surface with the dimensions of lon, 
-    lat. 
-    Units in degrees for angles, km for distances. 
-    Default radius for latitude theta and longitude 
-    phi. 
+    Function to calculate the curvature relative to a 
+    flat surface at the given radius for an ellipsoid 
+    defined by wgs84. Inputs include arrays of latitude, 
+    longitude, and a radius. Function returns array of 
+    depths relative to this flat surface with the 
+    dimensions of lon, lat. 
+    Units in degrees for angles, distances same as radius. 
+    Default radius calculated at default geographic theta. 
     """
     # preallocate output array
     curvature = np.zeros((len(lon), \
@@ -85,7 +82,7 @@ def get_curvature_wgs84(lat, lon, radius = 6370.287272978241, \
     for i in range(len(lon)):
         for j in range(len(lat)):
             # find radius at j-th latitude
-            if round(radius, 3) == radius_cnt(theta)/1000: 
+            if round(radius/1000, 3) == radius_cnt(theta)/1000: 
                 # when look at the surface curvature
                 # centred around default lat, lon
                 a = wgs84()[0]
@@ -112,7 +109,7 @@ def get_curvature_wgs84(lat, lon, radius = 6370.287272978241, \
             # calculate depth to curve from flat surface
             y = radius/np.cos(alpha) - radius_j
             x = y*np.cos(alpha)
-            curvature [i,j] = x 
+            curvature [i,j] = x*(-1) 
     
     # Cannot seem to find reason why curvature !=0 at 
     # (theta, phi), so just substituting that value 
@@ -160,9 +157,9 @@ def get_nc_curvature(filename, curvature_variable, x_var, y_var):
     today = dt.datetime.now()
     f.history = "Created " + today.strftime("%d/%m/%y")
     #Add local attributes to variable instances
-    curvature.units = 'km'
-    x.units = 'km'
-    y.units = 'km'
+    curvature.units = 'm'
+    x.units = 'm'
+    y.units = 'm'
 
     f.close()
 
@@ -279,7 +276,7 @@ def get_cartesian_distance(lon, lat, \
     return(x,y)
 
 def plot_curvature(lat, lon, curvature, src_lat = 37.5, \
-    src_lon = -16.5, cbar_label = 'Curvature (km)', \
+    src_lon = -16.5, cbar_label = 'Curvature (m)', \
     filename = 'noname'):
     """
     Function to plot a 3d surface once transformed 
@@ -299,7 +296,7 @@ def plot_curvature(lat, lon, curvature, src_lat = 37.5, \
     ax = plt.gca(projection = '3d')
     # TODO how to scale the axes, so z not so exaggerated
     # Plot 
-    surf = ax.plot_surface(x, y, curvature*(-1), \
+    surf = ax.plot_surface(x, y, curvature, \
         cmap = 'viridis')
     # Add colorbar
     cbar = fig.colorbar(surf, shrink = 0.5, aspect = 5)

benchmark/dom_calculations/get_curvature.py
@@ -23,10 +23,10 @@ bathy_Prt = np.ma.getdata(bathy_Prt)
 # Make the domain below for calculating curvature 
 # slightly bigger to have enough data (along lon)
 # That way there is enough data here to cover all
-lat_max = 39.5
-lat_min = 35.5
-lon_max = -13.8
-lon_min = -19.2
+lat_max = 40.0
+lat_min = 35.0
+lon_max = -13.5
+lon_min = -19.5
 bounds = [lat_max, lat_min, lon_max, lon_min]
 
 (lat_dom, lon_dom, bathy_dom) = truncate_domain(lat_Prt, \
@@ -39,13 +39,13 @@ from benchmark.dom_calculations.functions import \
     get_curvature
 
 surface_sphere = get_curvature(lat_dom, \
-    lon_dom, radius = 6370.107295)
+    lon_dom, radius = 6370287.273)
 ocean_sphere = get_curvature(lat_dom, \
-    lon_dom, radius = 6365.387295)
+    lon_dom, radius = 6365567.273)
 Moho_sphere = get_curvature(lat_dom, \
-    lon_dom, radius = 6357.937295)
+    lon_dom, radius = 6358117.273)
 bottom_sphere = get_curvature(lat_dom, \
-    lon_dom, radius = 6270.107295)
+    lon_dom, radius = 6270287.273)
 
 # %% Save curvature values for the spherical case
 from netCDF4 import Dataset
@@ -79,15 +79,16 @@ from benchmark.dom_calculations.functions import \
     get_curvature_wgs84
 
 # BUG in getting cartesian distances for ellipsoid case
+# BUG in curv_wgs84, don't get 0 at the centre
 
 surface_ellipsoid = get_curvature_wgs84(lat_dom, \
-    lon_dom, radius = 6370.287273)
+    lon_dom, radius = 6370287.273) - 32.4975
 ocean_ellipsoid = get_curvature_wgs84(lat_dom, \
-    lon_dom, radius = 6365.387295)
+    lon_dom, radius = 6365567.273) - 32.4734
 Moho_ellipsoid = get_curvature_wgs84(lat_dom, \
-    lon_dom, radius = 6357.937295)
+    lon_dom, radius = 6358117.273) - 32.4353
 bottom_ellipsoid = get_curvature_wgs84(lat_dom, \
-    lon_dom, radius = 6270.107295)
+    lon_dom, radius = 6270287.273) - 31.9873
 
 # %% Save curvature files for oblate Earth
 from netCDF4 import Dataset

coordinate_transformation/functions/transform.py
@@ -47,14 +47,14 @@ def get_cartesian_distance(lon, lat, \
     Calculate distance of each point of lat and lon
     from the source location on a flat surface, 
     tangential to the source. Returns x (lon), y 
-    (lat) in km for AxiSEMCartesian. 
+    (lat) in m for AxiSEMCartesian. 
     """
     # transform to geocentric
     lat = geographic_to_geocentric(lat)
     src_lat = geographic_to_geocentric(src_lat)
 
     # find radius at source
-    r_greatcircle = radius_cnt(src_lat)/1000
+    r_greatcircle = radius_cnt(src_lat)
     # find radius of small circle at source lat
     r_smallcircle = r_greatcircle*np.cos(np.deg2rad(src_lat))
     # convert differences in angles to radians

coordinate_transformation/prep_data/prep_bathy.py
@@ -33,11 +33,16 @@ raw_elevation = np.ma.getdata(raw_elevation)
 # %% Truncate, transform, rotate
 from coordinate_transformation.functions.get_rotation \
     import get_cartesian_distance
+from coordinate_transformation.functions.domain \
+    import get_variable
 # Define domain
 # Find indices for 35.5...39.5N, -14..-19E 
 # Made slightly bigger below to cover for sure the whole area with topography
-lat_max = 40
-lat_min = 35
+# Make the domain below for calculating bathymetry 
+# slightly bigger to have enough data 
+# That way there is enough data here to cover all
+lat_max = 40.0
+lat_min = 35.0
 lon_max = -13.5
 lon_min = -19.5
 bounds = [lat_max, lat_min, lon_max, lon_min]
@@ -50,10 +55,17 @@ bathy_Prt = bathy_Prt.transpose()
 (x_N, y_N) = get_cartesian_distance(lat_Prt, lon_Prt)
 # TODO check which way the input arguments have to be 
 
-# Express bathymetry as depth from reference level (4.72km) in km
-# positive downwards (i.e. deeper)
+# Express bathymetry as depth from reference level (4.72km) in m
+# positive upwards (i.e. shallower)
 
-rel_bathymetry = 4.72*(-1) - bathy_Prt/1000
+rel_bathymetry = (4720.0*(-1) - bathy_Prt)*(-1)
+
+# So far works for the Cartesian case but to add bathymetry to 
+# the geographic runs, need to make it relative to the curved
+# surface: 
+ocean_ellipsoid = get_variable('ocean_ellipsoid', \
+    'coordinate_transformation/variables/')
+rel_bathymetry = rel_bathymetry + ocean_ellipsoid
 
 # %% Save data as netCDF files
 
@@ -76,9 +88,9 @@ z [:] = rel_bathymetry
 today = dt.datetime.now()
 f.history = "Created " + today.strftime("%d/%m/%y")
 #Add local attributes to variable instances
-x.units = 'km'
-y.units = 'km'
-z.units = 'km'
+x.units = 'm'
+y.units = 'm'
+z.units = 'm'
 # Close file
 f.close()
 